2b5bb37075f1f02ed15a5e04bb49f3fbfab32681
hakan <hakan@hakan>
2026-02-21 17:58:54 +0300

    Start 2026

--- footer.php
+++ footer.php
@@ -2,6 +2,7 @@
 <div class="footer-area section-space--ptb_80">
 <div class="container">
 <div class="row footer-widget-wrapper">
+
 <div class="col-lg-4 col-md-6 col-sm-6 footer-widget">
 <div class="footer-widget__logo mb-30">
 <img src="assets/images/logo/dark-logo-160x48.webp" width="160" height="48" class="img-fluid" alt="">
@@ -13,6 +14,7 @@
 <li><a href="https://hasthemes.com/" class="hover-style-link text-color-primary">www.mitech.xperts.com</a></li>
 </ul>
 </div>
+
 <div class="col-lg-2 col-md-4 col-sm-6 footer-widget">
 <h6 class="footer-widget__title mb-20">IT Services</h6>
 <ul class="footer-widget__list">
@@ -23,6 +25,7 @@
 <li><a href="#" class="hover-style-link">Cyber Security</a></li>
 </ul>
 </div>
+
 <div class="col-lg-2 col-md-4 col-sm-6 footer-widget">
 <h6 class="footer-widget__title mb-20">Quick links</h6>
 <ul class="footer-widget__list">
@@ -32,6 +35,7 @@
 <li><a href="#" class="hover-style-link">Where to Find Us</a></li>
 </ul>
 </div>
+
 <div class="col-lg-2 col-md-4 col-sm-6 footer-widget">
 <h6 class="footer-widget__title mb-20">Support</h6>
 <ul class="footer-widget__list">
@@ -42,6 +46,7 @@
 <li><a href="#" class="hover-style-link">Cookies Policy</a></li>
 </ul>
 </div>
+
 <div class="col-lg-2 col-md-4 col-sm-6 footer-widget">
 <div class="footer-widget__title section-space--mb_50"></div>
 <ul class="footer-widget__list">
@@ -49,14 +54,16 @@
 <li><a href="#" class="image_btn" aria-label="App Store Button"><img class="img-fluid" src="assets/images/icons/aeroland-button-app-store.webp" alt=""></a></li>
 </ul>
 </div>
+
 </div>
 </div>
 </div>
+
 <div class="footer-copyright-area section-space--pb_30">
 <div class="container">
 <div class="row align-items-center">
 <div class="col-md-6 text-center text-md-start">
-<span class="copyright-text">&copy; 2023 Mitech. <a href="https://hasthemes.com/">All Rights Reserved.</a></span>
+<span class="copyright-text">&copy; <?php echo date("Y"); ?> <a href="https://www.cozumcloud.com.tr">[Çözüm Cloud]</a> Çanakkale Web Yazılım. Tüm hakları saklıdır.</span>
 </div>
 <div class="col-md-6 text-center text-md-end">
 <ul class="list ht-social-networks solid-rounded-icon">
@@ -86,4 +93,5 @@
 </div>
 </div>
 </div>
+
 </div>
