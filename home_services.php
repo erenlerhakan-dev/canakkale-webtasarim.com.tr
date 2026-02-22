f481cf1052123deebe956c4af206a35b9cb5d922
hakan <hakan@hakan>
2026-02-22 19:14:05 +0300

    start 2026

---
+++ home_services.php
@@ -0,0 +1,212 @@
+<div class="feature-icon-wrapper section-space--ptb_100">
+<div class="container">
+
+<div class="row">
+<div class="col-lg-12">
+<div class="section-title-wrap text-center section-space--mb_40">
+<h6 class="section-sub-title mb-20">Hizmetlerimiz</h6>
+<h3 class="heading"><span class="text-color-primary"> İş ihtiyaçlarınıza</span> göre uyarlanmış kapsamlı yazılım geliştirme ve bilişim çözümleri</h3>
+</div>
+</div>
+</div>
+
+<div class="row">
+
+<div class="col-12">
+<div class="feature-list__two">
+<div class="row">
+
+<div class="col-lg-4 col-md-6 wow move-up">
+<div class="ht-box-icon style-02 single-svg-icon-box">
+<div class="icon-box-wrap">
+<div class="icon">
+<div class="svg-icon" id="svg-1" data-svg-icon="assets/images/svg/linea-basic-heart.svg"></div>
+</div>
+<div class="content">
+<h5 class="heading">Özel Web Geliştirme</h5>
+<div class="text">React, Angular, Laravel ve Django gibi modern teknolojileri kullanarak özel iş ihtiyaçlarınıza göre özel web uygulamaları oluşturuyoruz.</div>
+<div class="feature-btn"><a href="#"><span class="button-text">Detay</span><i class="fas fa-arrow-right"></i></a></div>
+</div>
+</div>
+</div>
+</div>
+
+<div class="col-lg-4 col-md-6 wow move-up">
+<div class="ht-box-icon style-02 single-svg-icon-box">
+<div class="icon-box-wrap">
+<div class="icon">
+<div class="svg-icon" id="svg-2" data-svg-icon="assets/images/svg/linea-basic-case.svg"></div>
+</div>
+<div class="content">
+<h5 class="heading">CRM Sistemleri</h5>
+<div class="text">Müşteri etkileşimlerinizi iyileştirmek ve satış verimliliğini artırmak için tasarlanmış özel Müşteri İlişkileri Yönetimi sistemleri.</div>
+<div class="feature-btn"><a href="#"><span class="button-text">Detay</span><i class="fas fa-arrow-right"></i></a></div>
+</div>
+</div>
+</div>
+</div>
+
+<div class="col-lg-4 col-md-6 wow move-up">
+<div class="ht-box-icon style-02 single-svg-icon-box">
+<div class="icon-box-wrap">
+<div class="icon">
+<div class="svg-icon" id="svg-3" data-svg-icon="assets/images/svg/linea-basic-alarm.svg"></div>
+</div>
+<div class="content">
+<h5 class="heading">ERP Çözümleri</h5>
+<div class="text">İşletme operasyonlarınızın tüm yönlerini tek bir birleşik platforma entegre eden Kurumsal Kaynak Planlama sistemleri.</div>
+<div class="feature-btn"><a href="#"><span class="button-text">Detay</span><i class="fas fa-arrow-right"></i></a></div>
+</div>
+</div>
+</div>
+</div>
+
+<div class="col-lg-4 col-md-6 wow move-up">
+<div class="ht-box-icon style-02 single-svg-icon-box">
+<div class="icon-box-wrap">
+<div class="icon">
+<div class="svg-icon" id="svg-4" data-svg-icon="assets/images/svg/linea-basic-geolocalize-05.svg"></div>
+</div>
+<div class="content">
+<h5 class="heading">E-Ticaret Çözümleri</h5>
+<div class="text">Güvenli ödeme entegrasyonu, envanter yönetimi ve kullanıcı dostu alışveriş deneyimleri ile tam özellikli online mağazalar.</div>
+<div class="feature-btn"><a href="#"><span class="button-text">Detay</span><i class="fas fa-arrow-right"></i></a></div>
+</div>
+</div>
+</div>
+</div>
+
+<div class="col-lg-4 col-md-6 wow move-up">
+<div class="ht-box-icon style-02 single-svg-icon-box">
+<div class="icon-box-wrap">
+<div class="icon">
+<div class="svg-icon" id="svg-5" data-svg-icon="assets/images/svg/linea-ecommerce-money.svg"></div>
+</div>
+<div class="content">
+<h5 class="heading">Mobil Uygulamalar</h5>
+<div class="text">Olağanüstü kullanıcı deneyimleri sunan iOS ve Android için native ve cross-platform mobil uygulamalar.</div>
+<div class="feature-btn"><a href="#"><span class="button-text">Detay</span><i class="fas fa-arrow-right"></i></a></div>
+</div>
+</div>
+</div>
+</div>
+
+<div class="col-lg-4 col-md-6 wow move-up">
+<div class="ht-box-icon style-02 single-svg-icon-box">
+<div class="icon-box-wrap">
+<div class="icon">
+<div class="svg-icon" id="svg-6" data-svg-icon="assets/images/svg/linea-basic-spread-text-bookmark.svg"></div>
+</div>
+<div class="content">
+<h5 class="heading">Rezervasyon Sistemleri</h5>
+<div class="text">Oteller, restoranlar, hizmetler ve etkinlik yönetimi için özel rezervasyon ve randevu sistemleri.</div>
+<div class="feature-btn"><a href="#"><span class="button-text">Detay</span><i class="fas fa-arrow-right"></i></a></div>
+</div>
+</div>
+</div>
+</div>
+
+<div class="col-lg-4 col-md-6 wow move-up">
+<div class="ht-box-icon style-02 single-svg-icon-box">
+<div class="icon-box-wrap">
+<div class="icon">
+<div class="svg-icon" id="svg-6" data-svg-icon="assets/images/svg/linea-basic-spread-text-bookmark.svg"></div>
+</div>
+<div class="content">
+<h5 class="heading">Özel Yazılım Geliştirme</h5>
+<div class="text">Benzersiz iş zorluklarınızı ve gereksinimlerinizi ele almak için sıfırdan oluşturulmuş özel yazılım çözümleri.</div>
+<div class="feature-btn"><a href="#"><span class="button-text">Detay</span><i class="fas fa-arrow-right"></i></a></div>
+</div>
+</div>
+</div>
+</div>
+
+<div class="col-lg-4 col-md-6 wow move-up">
+<div class="ht-box-icon style-02 single-svg-icon-box">
+<div class="icon-box-wrap">
+<div class="icon">
+<div class="svg-icon" id="svg-6" data-svg-icon="assets/images/svg/linea-basic-spread-text-bookmark.svg"></div>
+</div>
+<div class="content">
+<h5 class="heading">B2B & B2C Çözümleri</h5>
+<div class="text">Gelişmiş özelliklerle işletmeden işletmeye ve işletmeden tüketiciye işlemler için özel platformlar.</div>
+<div class="feature-btn"><a href="#"><span class="button-text">Detay</span><i class="fas fa-arrow-right"></i></a></div>
+</div>
+</div>
+</div>
+</div>
+
+<div class="col-lg-4 col-md-6 wow move-up">
+<div class="ht-box-icon style-02 single-svg-icon-box">
+<div class="icon-box-wrap">
+<div class="icon">
+<div class="svg-icon" id="svg-6" data-svg-icon="assets/images/svg/linea-basic-spread-text-bookmark.svg"></div>
+</div>
+<div class="content">
+<h5 class="heading">API Geliştirme</h5>
+<div class="text">Farklı sistemler ve uygulamalar arasında sorunsuz entegrasyon sağlayan RESTful API'ler ve mikroservisler.</div>
+<div class="feature-btn"><a href="#"><span class="button-text">Detay</span><i class="fas fa-arrow-right"></i></a></div>
+</div>
+</div>
+</div>
+</div>
+
+<div class="col-lg-4 col-md-6 wow move-up">
+<div class="ht-box-icon style-02 single-svg-icon-box">
+<div class="icon-box-wrap">
+<div class="icon">
+<div class="svg-icon" id="svg-6" data-svg-icon="assets/images/svg/linea-basic-spread-text-bookmark.svg"></div>
+</div>
+<div class="content">
+<h5 class="heading">Güvenlik ve Bakım</h5>
+<div class="text">Uygulamalarınızın sorunsuz çalışmasını sağlamak için kapsamlı güvenlik denetimleri, düzenli güncellemeler ve sürekli bakım.</div>
+<div class="feature-btn"><a href="#"><span class="button-text">Detay</span><i class="fas fa-arrow-right"></i></a></div>
+</div>
+</div>
+</div>
+</div>
+
+<div class="col-lg-4 col-md-6 wow move-up">
+<div class="ht-box-icon style-02 single-svg-icon-box">
+<div class="icon-box-wrap">
+<div class="icon">
+<div class="svg-icon" id="svg-6" data-svg-icon="assets/images/svg/linea-basic-spread-text-bookmark.svg"></div>
+</div>
+<div class="content">
+<h5 class="heading">Performans Optimizasyonu</h5>
+<div class="text">Performans optimizasyonu, kod yeniden düzenleme ve altyapı iyileştirmeleriyle mevcut uygulamalarınızı geliştirin.</div>
+<div class="feature-btn"><a href="#"><span class="button-text">Detay</span><i class="fas fa-arrow-right"></i></a></div>
+</div>
+</div>
+</div>
+</div>
+
+<div class="col-lg-4 col-md-6 wow move-up">
+<div class="ht-box-icon style-02 single-svg-icon-box">
+<div class="icon-box-wrap">
+<div class="icon">
+<div class="svg-icon" id="svg-6" data-svg-icon="assets/images/svg/linea-basic-spread-text-bookmark.svg"></div>
+</div>
+<div class="content">
+<h5 class="heading">Teknik Danışmanlık</h5>
+<div class="text">Projeleriniz için teknoloji yığını seçimi, mimari tasarım ve en iyi uygulamalar konusunda uzman rehberlik.</div>
+<div class="feature-btn"><a href="#"><span class="button-text">Detay</span><i class="fas fa-arrow-right"></i></a></div>
+</div>
+</div>
+</div>
+</div>
+
+</div>
+</div>
+</div>
+
+<div class="col-lg-12">
+<div class="feature-list-button-box mt-30 text-center">
+<a href="#" class="ht-btn ht-btn-md">Talk to a consultant</a>
+<a href="#" class="ht-btn ht-btn-md ht-btn--outline">Contact us now </a>
+</div>
+</div>
+
+</div>
+</div>
+</div>
