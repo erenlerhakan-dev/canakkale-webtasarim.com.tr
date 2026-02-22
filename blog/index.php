2d2068a9d84f367c151d366823bb7e24e7da88eb
hakan <hakan@hakan>
2026-02-22 22:30:52 +0300

    start 2026

--- blog/index.php
+++ blog/index.php
@@ -75,6 +75,314 @@
 </div>
 </div>
 
+<div class="single-blog-item post-list-wrapper xs-list-blog-item border-bottom wow move-up">
+<div class="row ">
+
+<div class="col-lg-6">
+
+<div class="post-feature blog-thumbnail">
+<a href="/blog/aaa.php">
+<img class="img-fluid" src="/assets/images/blog/blog-01-540x330.webp" alt="2024'te Modern Web Geliştirme Trendleri">
+</a>
+</div>
+
+</div>
+
+<div class="col-lg-6">
+
+<div class="post-info lg-blog-post-info">
+
+<h4 class="post-title">
+<a href="/blog/aaa.php">Türk İşletmeleri için Özel CRM Sistemlerinin Faydaları</a>
+</h4>
+
+<div class="post-meta mt-20">
+<div class="post-author"><a href="https://www.hakanerenler.net"><span class="fa fa-user meta-icon"></span> Hakan Erenler</a></div>
+<div class="post-date"><span class="far fa-calendar meta-icon"></span>20 Ocak 2024</div>
+<div class="post-view"><span class="fa fa-list meta-icon"></span>CRM</div>
+</div>
+
+<div class="post-excerpt mt-15">
+<p>Özel CRM çözümlerinin işletme operasyonlarınızı ve müşteri ilişkilerinizi nasıl dönüştürebileceğini keşfedin.</p>
+</div>
+
+<div class="read-post-share-wrap mt-30">
+<div class="post-read-more">
+<a href="/blog/aaa.php" class="ht-btn ht-btn-md">Devamını oku </a>
+</div>
+</div>
+
+</div>
+
+</div>
+
+</div>
+</div>
+
+<div class="single-blog-item post-list-wrapper xs-list-blog-item border-bottom wow move-up">
+<div class="row ">
+
+<div class="col-lg-6">
+
+<div class="post-feature blog-thumbnail">
+<a href="/blog/aaa.php">
+<img class="img-fluid" src="/assets/images/blog/blog-01-540x330.webp" alt="2024'te Modern Web Geliştirme Trendleri">
+</a>
+</div>
+
+</div>
+
+<div class="col-lg-6">
+
+<div class="post-info lg-blog-post-info">
+
+<h4 class="post-title">
+<a href="/blog/aaa.php">Küçük İşletmeler için E-Ticaret Başarı Stratejileri</a>
+</h4>
+
+<div class="post-meta mt-20">
+<div class="post-author"><a href="https://www.hakanerenler.net"><span class="fa fa-user meta-icon"></span> Hakan Erenler</a></div>
+<div class="post-date"><span class="far fa-calendar meta-icon"></span>25 Ocak 2024</div>
+<div class="post-view"><span class="fa fa-list meta-icon"></span>E-Ticaret</div>
+</div>
+
+<div class="post-excerpt mt-15">
+<p>Türkiye pazarında başarılı bir e-ticaret platformu oluşturmak ve büyütmek için temel stratejiler.</p>
+</div>
+
+<div class="read-post-share-wrap mt-30">
+<div class="post-read-more">
+<a href="/blog/aaa.php" class="ht-btn ht-btn-md">Devamını oku </a>
+</div>
+</div>
+
+</div>
+
+</div>
+
+</div>
+</div>
+
+<div class="single-blog-item post-list-wrapper xs-list-blog-item border-bottom wow move-up">
+<div class="row ">
+
+<div class="col-lg-6">
+
+<div class="post-feature blog-thumbnail">
+<a href="/blog/aaa.php">
+<img class="img-fluid" src="/assets/images/blog/blog-01-540x330.webp" alt="2024'te Modern Web Geliştirme Trendleri">
+</a>
+</div>
+
+</div>
+
+<div class="col-lg-6">
+
+<div class="post-info lg-blog-post-info">
+
+<h4 class="post-title">
+<a href="/blog/aaa.php">React vs Angular: Doğru Frontend Framework'ü Seçmek</a>
+</h4>
+
+<div class="post-meta mt-20">
+<div class="post-author"><a href="https://www.hakanerenler.net"><span class="fa fa-user meta-icon"></span> Hakan Erenler</a></div>
+<div class="post-date"><span class="far fa-calendar meta-icon"></span>5 Şubat 2024</div>
+<div class="post-view"><span class="fa fa-list meta-icon"></span>Geliştirme</div>
+</div>
+
+<div class="post-excerpt mt-15">
+<p>Projeniz için en iyi frontend framework'ünü seçmenize yardımcı olacak kapsamlı karşılaştırma.</p>
+</div>
+
+<div class="read-post-share-wrap mt-30">
+<div class="post-read-more">
+<a href="/blog/aaa.php" class="ht-btn ht-btn-md">Devamını oku </a>
+</div>
+</div>
+
+</div>
+
+</div>
+
+</div>
+</div>
+
+<div class="single-blog-item post-list-wrapper xs-list-blog-item border-bottom wow move-up">
+<div class="row ">
+
+<div class="col-lg-6">
+
+<div class="post-feature blog-thumbnail">
+<a href="/blog/aaa.php">
+<img class="img-fluid" src="/assets/images/blog/blog-01-540x330.webp" alt="2024'te Modern Web Geliştirme Trendleri">
+</a>
+</div>
+
+</div>
+
+<div class="col-lg-6">
+
+<div class="post-info lg-blog-post-info">
+
+<h4 class="post-title">
+<a href="/blog/aaa.php">Laravel Neden Kurumsal Uygulamalar İçin Mükemmeldir</a>
+</h4>
+
+<div class="post-meta mt-20">
+<div class="post-author"><a href="https://www.hakanerenler.net"><span class="fa fa-user meta-icon"></span> Hakan Erenler</a></div>
+<div class="post-date"><span class="far fa-calendar meta-icon"></span>1 Şubat 2024</div>
+<div class="post-view"><span class="fa fa-list meta-icon"></span>Web Geliştirme</div>
+</div>
+
+<div class="post-excerpt mt-15">
+<p>Laravel'in kurumsal düzeyde yazılım geliştirme için ideal kılan güçlü özelliklerini anlayın.</p>
+</div>
+
+<div class="read-post-share-wrap mt-30">
+<div class="post-read-more">
+<a href="/blog/aaa.php" class="ht-btn ht-btn-md">Devamını oku </a>
+</div>
+</div>
+
+</div>
+
+</div>
+
+</div>
+</div>
+
+<div class="single-blog-item post-list-wrapper xs-list-blog-item border-bottom wow move-up">
+<div class="row ">
+
+<div class="col-lg-6">
+
+<div class="post-feature blog-thumbnail">
+<a href="/blog/aaa.php">
+<img class="img-fluid" src="/assets/images/blog/blog-01-540x330.webp" alt="2024'te Modern Web Geliştirme Trendleri">
+</a>
+</div>
+
+</div>
+
+<div class="col-lg-6">
+
+<div class="post-info lg-blog-post-info">
+
+<h4 class="post-title">
+<a href="/blog/aaa.php">Yüksek Performanslı Uygulamalar İçin Veritabanı Optimizasyon Teknikleri</a>
+</h4>
+
+<div class="post-meta mt-20">
+<div class="post-author"><a href="https://www.hakanerenler.net"><span class="fa fa-user meta-icon"></span> Hakan Erenler</a></div>
+<div class="post-date"><span class="far fa-calendar meta-icon"></span>10 Şubat 2024</div>
+<div class="post-view"><span class="fa fa-list meta-icon"></span>Web Geliştirme</div>
+</div>
+
+<div class="post-excerpt mt-15">
+<p>Uygulama performansını artırmak için temel veritabanı optimizasyon stratejilerini öğrenin.</p>
+</div>
+
+<div class="read-post-share-wrap mt-30">
+<div class="post-read-more">
+<a href="/blog/aaa.php" class="ht-btn ht-btn-md">Devamını oku </a>
+</div>
+</div>
+
+</div>
+
+</div>
+
+</div>
+</div>
+
+<div class="single-blog-item post-list-wrapper xs-list-blog-item border-bottom wow move-up">
+<div class="row ">
+
+<div class="col-lg-6">
+
+<div class="post-feature blog-thumbnail">
+<a href="/blog/aaa.php">
+<img class="img-fluid" src="/assets/images/blog/blog-01-540x330.webp" alt="2024'te Modern Web Geliştirme Trendleri">
+</a>
+</div>
+
+</div>
+
+<div class="col-lg-6">
+
+<div class="post-info lg-blog-post-info">
+
+<h4 class="post-title">
+<a href="/blog/aaa.php">Modern Web Siteleri İçin Mobil Öncelikli Tasarım Prensipleri</a>
+</h4>
+
+<div class="post-meta mt-20">
+<div class="post-author"><a href="https://www.hakanerenler.net"><span class="fa fa-user meta-icon"></span> Hakan Erenler</a></div>
+<div class="post-date"><span class="far fa-calendar meta-icon"></span>15 Şubat 2024</div>
+<div class="post-view"><span class="fa fa-list meta-icon"></span>Tasarım</div>
+</div>
+
+<div class="post-excerpt mt-15">
+<p>Duyarlı, mobil optimize edilmiş web deneyimleri oluşturmak için temel prensipler.</p>
+</div>
+
+<div class="read-post-share-wrap mt-30">
+<div class="post-read-more">
+<a href="/blog/aaa.php" class="ht-btn ht-btn-md">Devamını oku </a>
+</div>
+</div>
+
+</div>
+
+</div>
+
+</div>
+</div>
+
+<div class="single-blog-item post-list-wrapper xs-list-blog-item border-bottom wow move-up">
+<div class="row ">
+
+<div class="col-lg-6">
+
+<div class="post-feature blog-thumbnail">
+<a href="/blog/aaa.php">
+<img class="img-fluid" src="/assets/images/blog/blog-01-540x330.webp" alt="2024'te Modern Web Geliştirme Trendleri">
+</a>
+</div>
+
+</div>
+
+<div class="col-lg-6">
+
+<div class="post-info lg-blog-post-info">
+
+<h4 class="post-title">
+<a href="/blog/aaa.php">Modern Web Siteleri İçin Mobil Öncelikli Tasarım Prensipleri</a>
+</h4>
+
+<div class="post-meta mt-20">
+<div class="post-author"><a href="https://www.hakanerenler.net"><span class="fa fa-user meta-icon"></span> Hakan Erenler</a></div>
+<div class="post-date"><span class="far fa-calendar meta-icon"></span>15 Şubat 2024</div>
+<div class="post-view"><span class="fa fa-list meta-icon"></span>Tasarım</div>
+</div>
+
+<div class="post-excerpt mt-15">
+<p>Duyarlı, mobil optimize edilmiş web deneyimleri oluşturmak için temel prensipler.</p>
+</div>
+
+<div class="read-post-share-wrap mt-30">
+<div class="post-read-more">
+<a href="/blog/aaa.php" class="ht-btn ht-btn-md">Devamını oku </a>
+</div>
+</div>
+
+</div>
+
+</div>
+
+</div>
+</div>
+
 </div>
 
 </div>
