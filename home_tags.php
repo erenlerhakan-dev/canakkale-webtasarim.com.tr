7395222e8f0fb167c88ff76e6e5836c3232c7a02
hakan <hakan@hakan>
2026-02-22 18:52:06 +0300

    start 2026

--- home_tags.php
+++ home_tags.php
@@ -8,12 +8,18 @@
 <p>Hizmetlerimiz ve süreçlerimiz hakkında yaygın sorular</p>
 </div>
 
-<a href="#" class="ht-btn ht-btn-md ht-btn--outline">Solid Style</a>
-<a href="#" class="ht-btn ht-btn-md ht-btn--outline">Solid Style</a>
-<a href="#" class="ht-btn ht-btn-md ht-btn--outline">Solid Style</a>
-<a href="#" class="ht-btn ht-btn-md ht-btn--outline">Solid Style</a>
-<a href="#" class="ht-btn ht-btn-md ht-btn--outline">Solid Style</a>
-<a href="#" class="ht-btn ht-btn-md ht-btn--outline">Solid Style</a>
+<a href="#" class="ht-btn ht-btn-md ht-btn--outline">HTML5 & CSS3</a>
+<a href="#" class="ht-btn ht-btn-md ht-btn--outline">JavaScript</a>
+<a href="#" class="ht-btn ht-btn-md ht-btn--outline">React</a>
+<a href="#" class="ht-btn ht-btn-md ht-btn--outline">Angular</a>
+<a href="#" class="ht-btn ht-btn-md ht-btn--outline">PHP</a>
+<a href="#" class="ht-btn ht-btn-md ht-btn--outline">Laravel</a>
+<a href="#" class="ht-btn ht-btn-md ht-btn--outline">Python</a>
+<a href="#" class="ht-btn ht-btn-md ht-btn--outline">Django</a>
+<a href="#" class="ht-btn ht-btn-md ht-btn--outline">Ruby on Rails</a>
+<a href="#" class="ht-btn ht-btn-md ht-btn--outline">Java</a>
+<a href="#" class="ht-btn ht-btn-md ht-btn--outline">SQL</a>
+<a href="#" class="ht-btn ht-btn-md ht-btn--outline">Node.js</a>
 
 </div>
 
