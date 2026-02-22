929e738680f8e260d8ff46de143a424f669c036d
hakan <hakan@hakan>
2026-02-22 22:15:28 +0300

    start 2026

---
+++ blog/index.php
@@ -0,0 +1,479 @@
+<!DOCTYPE html>
+<html class="no-js" lang="tr" dir="ltr">
+
+<head>
+<title>Çanakkale Web Yazılım - Profesyonel Web Geliştirme ve Yazılım Çözümleri</title>
+<meta name="description" content="Çanakkale Web Yazılım - Profesyonel Web Geliştirme ve Yazılım Çözümleri. Çanakkale, Türkiye'de profesyonel web geliştirme ve yazılım çözümleri. CRM, ERP, E-ticaret, Rezervasyon Sistemleri ve Laravel, React, Django ve daha fazlasıyla özel yazılım geliştirme.">
+
+<?php include_once('../head_meta.php'); ?>
+
+</head>
+
+<body>
+
+<?php include_once('../preloader.php'); ?>
+
+<div class="header-area header-area--default">
+
+<?php include_once('../header.php'); ?>
+
+<div class="breadcrumb-area">
+<div class="container">
+<div class="row">
+<div class="col-12">
+<div class="breadcrumb_box text-center">
+<h2 class="breadcrumb-title">Blog Update</h2>
+<ul class="breadcrumb-list">
+<li class="breadcrumb-item"><a href="index.html">Home</a></li>
+<li class="breadcrumb-item active">List Left Large Image</li>
+</ul>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+<div id="main-wrapper">
+
+<div class="site-wrapper-reveal">
+
+
+
+<!--==================== Blog Area Start ====================-->
+<div class="blog-pages-wrapper section-space--ptb_100">
+<div class="container">
+<div class="row">
+
+<div class="col-lg-12 order-lg-2 order-1">
+<div class="section-title-wrap text-center section-space--mb_60">
+<h3 class="heading">Interesting articles<span class="text-color-primary"> updated daily</span></h3>
+</div>
+<div class="main-blog-wrap">
+<!--======= Single Blog Item Start ========-->
+<div class="single-blog-item post-list-wrapper xs-list-blog-item border-bottom wow move-up">
+<div class="row ">
+<div class="col-lg-6">
+<!-- Post Feature Start -->
+<div class="post-feature blog-thumbnail">
+<a href="blog-post-layout-one.html">
+<img class="img-fluid" src="assets/images/blog/blog-01-540x330.webp" alt="Blog Images">
+</a>
+</div>
+<!-- Post Feature End -->
+</div>
+
+<div class="col-lg-6">
+<!-- Post info Start -->
+<div class="post-info lg-blog-post-info">
+<h4 class="post-title">
+<a href="blog-post-layout-one.html">Ideas for High Returns on Investment</a>
+</h4>
+
+<div class="post-meta mt-20">
+<div class="post-author">
+<a href="#">
+<img class="img-fluid avatar-96" src="assets/images/team/admin.webp" alt=""> Harry Ferguson
+</a>
+</div>
+<div class="post-date">
+<span class="far fa-calendar meta-icon"></span>
+January 14, 2019
+</div>
+<div class="post-view">
+<span class="meta-icon far fa-eye"></span>
+346 views
+</div>
+</div>
+
+<div class="post-excerpt mt-15">
+<p>Using the profit accumulator is one such way, and you should understand why it increases your chances of the profits. …</p>
+</div>
+
+
+<div class="read-post-share-wrap mt-30">
+<div class="post-read-more">
+<a href="blog-post-layout-one.html" class="ht-btn ht-btn-md">Read more </a>
+</div>
+</div>
+</div>
+<!-- Post info End -->
+</div>
+</div>
+</div>
+<!--===== Single Blog Item End =========-->
+
+<!--======= Single Blog Item Start ========-->
+<div class="single-blog-item post-list-wrapper xs-list-blog-item border-bottom wow move-up">
+<div class="row ">
+<div class="col-lg-6">
+<!-- Post Feature Start -->
+<div class="post-feature blog-thumbnail">
+<a href="blog-post-layout-one.html">
+<img class="img-fluid" src="assets/images/blog/blog-08-540x330.webp" alt="Blog Images">
+</a>
+</div>
+<!-- Post Feature End -->
+</div>
+
+<div class="col-lg-6">
+<!-- Post info Start -->
+<div class="post-info lg-blog-post-info">
+<h4 class="post-title">
+<a href="blog-post-layout-one.html">How Technology Made Businesses More Efficient</a>
+</h4>
+
+<div class="post-meta mt-20">
+<div class="post-author">
+<a href="#">
+<img class="img-fluid avatar-96" src="assets/images/team/admin.webp" alt=""> James Scott
+</a>
+</div>
+<div class="post-date">
+<span class="far fa-calendar meta-icon"></span>
+January 14, 2019
+</div>
+<div class="post-view">
+<span class="meta-icon far fa-eye"></span>
+346 views
+</div>
+</div>
+
+<div class="post-excerpt mt-15">
+<p>Using the profit accumulator is one such way, and you should understand why it increases your chances of the profits. …</p>
+</div>
+
+
+<div class="read-post-share-wrap mt-30">
+<div class="post-read-more">
+<a href="blog-post-layout-one.html" class="ht-btn ht-btn-md">Read more </a>
+</div>
+</div>
+</div>
+<!-- Post info End -->
+</div>
+</div>
+</div>
+<!--===== Single Blog Item End =========-->
+
+<!--======= Single Blog Item Start ========-->
+<div class="single-blog-item post-list-wrapper xs-list-blog-item border-bottom wow move-up">
+<div class="row ">
+<div class="col-lg-6">
+<!-- Post Feature Start -->
+<div class="post-feature blog-thumbnail">
+<a href="blog-post-layout-one.html">
+<img class="img-fluid" src="assets/images/blog/blog-07-540x330.webp" alt="Blog Images">
+</a>
+</div>
+<!-- Post Feature End -->
+</div>
+
+<div class="col-lg-6">
+<!-- Post info Start -->
+<div class="post-info lg-blog-post-info">
+<h4 class="post-title">
+<a href="blog-post-layout-one.html">Data Secure on Transitioning to a New Office</a>
+</h4>
+
+<div class="post-meta mt-20">
+<div class="post-author">
+<a href="#">
+<img class="img-fluid avatar-96" src="assets/images/team/admin-02.webp" alt=""> James Scott
+</a>
+</div>
+<div class="post-date">
+<span class="far fa-calendar meta-icon"></span>
+January 14, 2019
+</div>
+<div class="post-view">
+<span class="meta-icon far fa-eye"></span>
+346 views
+</div>
+</div>
+
+<div class="post-excerpt mt-15">
+<p>Using the profit accumulator is one such way, and you should understand why it increases your chances of the profits. …</p>
+</div>
+
+
+<div class="read-post-share-wrap mt-30">
+<div class="post-read-more">
+<a href="blog-post-layout-one.html" class="ht-btn ht-btn-md">Read more </a>
+</div>
+</div>
+</div>
+<!-- Post info End -->
+</div>
+</div>
+</div>
+<!--===== Single Blog Item End =========-->
+
+<!--======= Single Blog Item Start ========-->
+<div class="single-blog-item post-list-wrapper xs-list-blog-item border-bottom wow move-up">
+<div class="row ">
+<div class="col-lg-6">
+<!-- Post Feature Start -->
+<div class="post-feature blog-thumbnail">
+<a href="blog-post-layout-one.html">
+<img class="img-fluid" src="assets/images/blog/blog-05-540x330.webp" alt="Blog Images">
+</a>
+</div>
+<!-- Post Feature End -->
+</div>
+
+<div class="col-lg-6">
+<!-- Post info Start -->
+<div class="post-info lg-blog-post-info">
+<h4 class="post-title">
+<a href="blog-post-layout-one.html">Blockchain Technology Effects on Logistics Sector?</a>
+</h4>
+
+<div class="post-meta mt-20">
+<div class="post-author">
+<a href="#">
+<img class="img-fluid avatar-96" src="assets/images/team/admin.webp" alt=""> James Scott
+</a>
+</div>
+<div class="post-date">
+<span class="far fa-calendar meta-icon"></span>
+January 14, 2019
+</div>
+<div class="post-view">
+<span class="meta-icon far fa-eye"></span>
+346 views
+</div>
+</div>
+
+<div class="post-excerpt mt-15">
+<p>Using the profit accumulator is one such way, and you should understand why it increases your chances of the profits. …</p>
+</div>
+
+
+<div class="read-post-share-wrap mt-30">
+<div class="post-read-more">
+<a href="blog-post-layout-one.html" class="ht-btn ht-btn-md">Read more </a>
+</div>
+</div>
+</div>
+<!-- Post info End -->
+</div>
+</div>
+</div>
+<!--===== Single Blog Item End =========-->
+
+<!--======= Single Blog Item Start ========-->
+<div class="single-blog-item post-list-wrapper xs-list-blog-item border-bottom wow move-up">
+<div class="row ">
+<div class="col-lg-6">
+<!-- Post Feature Start -->
+<div class="post-feature blog-thumbnail">
+<a href="blog-post-layout-one.html">
+<img class="img-fluid" src="assets/images/blog/blog-02-540x330.webp" alt="Blog Images">
+</a>
+</div>
+<!-- Post Feature End -->
+</div>
+
+<div class="col-lg-6">
+<!-- Post info Start -->
+<div class="post-info lg-blog-post-info">
+<h4 class="post-title">
+<a href="blog-post-layout-one.html">How Wireless Technology is Changing Business</a>
+</h4>
+
+<div class="post-meta mt-20">
+<div class="post-author">
+<a href="#">
+<img class="img-fluid avatar-96" src="assets/images/team/admin-02.webp" alt=""> James Scott
+</a>
+</div>
+<div class="post-date">
+<span class="far fa-calendar meta-icon"></span>
+January 14, 2019
+</div>
+<div class="post-view">
+<span class="meta-icon far fa-eye"></span>
+346 views
+</div>
+</div>
+
+<div class="post-excerpt mt-15">
+<p>Using the profit accumulator is one such way, and you should understand why it increases your chances of the profits. …</p>
+</div>
+
+
+<div class="read-post-share-wrap mt-30">
+<div class="post-read-more">
+<a href="blog-post-layout-one.html" class="ht-btn ht-btn-md">Read more </a>
+</div>
+</div>
+</div>
+<!-- Post info End -->
+</div>
+</div>
+</div>
+<!--===== Single Blog Item End =========-->
+
+<div class="ht-pagination mt-30 pagination justify-content-center">
+<div class="pagination-wrapper">
+<ul class="page-pagination">
+<li><a class="prev page-numbers" href="#">Prev</a></li>
+<li><a class="page-numbers current" href="#">1</a></li>
+<li><a class="page-numbers" href="#">2</a></li>
+<li><a class="next page-numbers" href="#">Next</a></li>
+</ul>
+</div>
+</div>
+
+</div>
+</div>
+</div>
+</div>
+</div>
+<!--==================== Blog Area End ====================-->
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!--========== Call to Action Area Start ============-->
+<div class="cta-image-area_one section-space--ptb_80 cta-bg-image_one">
+<div class="container">
+<div class="row align-items-center">
+<div class="col-xl-8 col-lg-7">
+<div class="cta-content md-text-center">
+<h3 class="heading text-white">Assess your business potentials and find opportunities <span class="text-color-secondary">for bigger success</span></h3>
+</div>
+</div>
+<div class="col-xl-4 col-lg-5">
+<div class="cta-button-group--one text-center">
+<a href="#" class="btn btn--white btn-one"><span class="btn-icon me-2"><i class="far fa-comment-alt"></i></span> Let's talk</a>
+<a href="#" class="btn btn--secondary btn-two"><span class="btn-icon me-2"><i class="fas fa-info-circle"></i></span> Get info</a>
+</div>
+</div>
+</div>
+</div>
+</div>
+<!--========== Call to Action Area End ============-->
+
+
+
+
+
+
+
+
+</div>
+
+<div class="footer-area-wrapper bg-gray">
+<div class="footer-area section-space--ptb_80">
+<div class="container">
+<div class="row footer-widget-wrapper">
+<div class="col-lg-4 col-md-6 col-sm-6 footer-widget">
+<div class="footer-widget__logo mb-30">
+<img src="assets/images/logo/dark-logo-160x48.webp" width="160" height="48" class="img-fluid" alt="">
+</div>
+<ul class="footer-widget__list">
+<li>58 Howard Street #2 San Francisco, CA 941</li>
+<li><a href="mailto:contact@aeroland.com" class="hover-style-link">contact@aeroland.com</a></li>
+<li><a href="tel:123344556" class="hover-style-link text-black font-weight--bold">(+68)1221 09876</a></li>
+<li><a href="https://hasthemes.com/" class="hover-style-link text-color-primary">www.mitech.xperts.com</a></li>
+</ul>
+</div>
+<div class="col-lg-2 col-md-4 col-sm-6 footer-widget">
+<h6 class="footer-widget__title mb-20">IT Services</h6>
+<ul class="footer-widget__list">
+<li><a href="#" class="hover-style-link">Managed IT</a></li>
+<li><a href="#" class="hover-style-link">IT Support</a></li>
+<li><a href="#" class="hover-style-link">IT Consultancy</a></li>
+<li><a href="#" class="hover-style-link">Cloud Computing</a></li>
+<li><a href="#" class="hover-style-link">Cyber Security</a></li>
+</ul>
+</div>
+<div class="col-lg-2 col-md-4 col-sm-6 footer-widget">
+<h6 class="footer-widget__title mb-20">Quick links</h6>
+<ul class="footer-widget__list">
+<li><a href="#" class="hover-style-link">Pick up locations</a></li>
+<li><a href="#" class="hover-style-link">Terms of Payment</a></li>
+<li><a href="#" class="hover-style-link">Privacy Policy</a></li>
+<li><a href="#" class="hover-style-link">Where to Find Us</a></li>
+</ul>
+</div>
+<div class="col-lg-2 col-md-4 col-sm-6 footer-widget">
+<h6 class="footer-widget__title mb-20">Support</h6>
+<ul class="footer-widget__list">
+<li><a href="#" class="hover-style-link">Forum Support</a></li>
+<li><a href="#" class="hover-style-link">Help & FAQ</a></li>
+<li><a href="#" class="hover-style-link">Contact Us</a></li>
+<li><a href="#" class="hover-style-link">Pricing and plans</a></li>
+<li><a href="#" class="hover-style-link">Cookies Policy</a></li>
+</ul>
+</div>
+<div class="col-lg-2 col-md-4 col-sm-6 footer-widget">
+<div class="footer-widget__title section-space--mb_50"></div>
+<ul class="footer-widget__list">
+<li><a href="#" class="image_btn" aria-label="Google play Button"><img class="img-fluid" src="assets/images/icons/aeroland-button-google-play.webp" alt=""></a></li>
+<li><a href="#" class="image_btn" aria-label="App Store Button"><img class="img-fluid" src="assets/images/icons/aeroland-button-app-store.webp" alt=""></a></li>
+</ul>
+</div>
+</div>
+</div>
+</div>
+<div class="footer-copyright-area section-space--pb_30">
+<div class="container">
+<div class="row align-items-center">
+<div class="col-md-6 text-center text-md-start">
+<span class="copyright-text">&copy; 2023 Mitech. <a href="https://hasthemes.com/">All Rights Reserved.</a></span>
+</div>
+<div class="col-md-6 text-center text-md-end">
+<ul class="list ht-social-networks solid-rounded-icon">
+
+<li class="item">
+<a href="https://twitter.com" target="_blank" aria-label="Twitter" class="social-link hint--bounce hint--top hint--primary">
+<i class="fab fa-twitter link-icon"></i>
+</a>
+</li>
+<li class="item">
+<a href="https://facebook.com" target="_blank" aria-label="Facebook" class="social-link hint--bounce hint--top hint--primary">
+<i class="fab fa-facebook-f link-icon"></i>
+</a>
+</li>
+<li class="item">
+<a href="https://instagram.com" target="_blank" aria-label="Instagram" class="social-link hint--bounce hint--top hint--primary">
+<i class="fab fa-instagram link-icon"></i>
+</a>
+</li>
+<li class="item">
+<a href="https://linkedin.com" target="_blank" aria-label="Linkedin" class="social-link hint--bounce hint--top hint--primary">
+<i class="fab fa-linkedin link-icon"></i>
+</a>
+</li>
+</ul>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+</div>
+
+<a href="#" class="scroll-top" id="scroll-top">
+<i class="arrow-top fas fa-chevron-up"></i>
+<i class="arrow-bottom fas fa-chevron-up"></i>
+</a>
+
+<?php include_once('../mobile_menu.php'); ?>
+
+<?php include_once('../footer_scripts.php'); ?>
+
+</body>
+
+</html>
