aefa5df24db1434dfc2285601e10067dc058ce22
hakan <hakan@hakan>
2026-02-22 18:48:18 +0300

    start 2026

--- home_tags.php
+++ home_tags.php
@@ -4,10 +4,6 @@
 
 <div class="col-lg-8 align-self-center">
 
-<div class="toggles-wrapper section-space--pt_90">
-
-<div class="faq-wrapper">
-
 <div class="section-elements-title mb-30 text-center">
 <h4>Sık Sorulan Sorular</h4>
 <p>Hizmetlerimiz ve süreçlerimiz hakkında yaygın sorular</p>
@@ -17,10 +13,6 @@
 <a class="tm-button style-solid tm-button-nm tm-button-" href="#">
 <span class="button-text" data-text="Solid Style">HTML5 & CSS3</span>
 </a>
-</div>
-
-</div>
-
 </div>
 
 </div>
