643ec8ea202863e122deb33df1b50c79ecad69e2
hakan <hakan@hakan>
2026-02-22 18:57:41 +0300

    start 2026

--- home_tags.php
+++ home_tags.php
@@ -8,18 +8,18 @@
 <p>Son teknoloji çözümler için modern teknoloji altyapıları</p>
 </div>
 
-<a href="#" class="ht-btn ht-btn-md ht-btn--outline hometags">HTML5 & CSS3</a>
-<a href="#" class="ht-btn ht-btn-md ht-btn--outline hometags">JavaScript</a>
-<a href="#" class="ht-btn ht-btn-md ht-btn--outline hometags">React</a>
-<a href="#" class="ht-btn ht-btn-md ht-btn--outline hometags">Angular</a>
-<a href="#" class="ht-btn ht-btn-md ht-btn--outline hometags">PHP</a>
-<a href="#" class="ht-btn ht-btn-md ht-btn--outline hometags">Laravel</a>
-<a href="#" class="ht-btn ht-btn-md ht-btn--outline hometags">Python</a>
-<a href="#" class="ht-btn ht-btn-md ht-btn--outline hometags">Django</a>
-<a href="#" class="ht-btn ht-btn-md ht-btn--outline hometags">Ruby on Rails</a>
-<a href="#" class="ht-btn ht-btn-md ht-btn--outline hometags">Java</a>
-<a href="#" class="ht-btn ht-btn-md ht-btn--outline hometags">SQL</a>
-<a href="#" class="ht-btn ht-btn-md ht-btn--outline hometags">Node.js</a>
+<a href="#" class="ht-btn ht-btn-sm ht-btn--outline hometags">HTML5 & CSS3</a>
+<a href="#" class="ht-btn ht-btn-sm ht-btn--outline hometags">JavaScript</a>
+<a href="#" class="ht-btn ht-btn-sm ht-btn--outline hometags">React</a>
+<a href="#" class="ht-btn ht-btn-sm ht-btn--outline hometags">Angular</a>
+<a href="#" class="ht-btn ht-btn-sm ht-btn--outline hometags">PHP</a>
+<a href="#" class="ht-btn ht-btn-sm ht-btn--outline hometags">Laravel</a>
+<a href="#" class="ht-btn ht-btn-sm ht-btn--outline hometags">Python</a>
+<a href="#" class="ht-btn ht-btn-sm ht-btn--outline hometags">Django</a>
+<a href="#" class="ht-btn ht-btn-sm ht-btn--outline hometags">Ruby on Rails</a>
+<a href="#" class="ht-btn ht-btn-sm ht-btn--outline hometags">Java</a>
+<a href="#" class="ht-btn ht-btn-sm ht-btn--outline hometags">SQL</a>
+<a href="#" class="ht-btn ht-btn-sm ht-btn--outline hometags">Node.js</a>
 
 </div>
 
