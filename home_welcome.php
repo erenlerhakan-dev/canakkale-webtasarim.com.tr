3dee99d6c019f350c3af90125f8a928a93136936
hakan <hakan@hakan>
2026-02-22 19:42:28 +0300

    start 2026

--- home_welcome.php
+++ home_welcome.php
@@ -5,30 +5,8 @@
 <div class="col-lg-6 col-md-6">
 <div class="appointment-hero-wrap wow move-up">
 <div class="appointment-hero-text">
-<h6 class="text-white">Secure & IT Services </h6>
-<h1 class="font-weight--reguler text-white mb-30">Excellent IT services for <span class="text-line">your success</span></h1>
-<div class="hero-button">
-<div class="video-popup infotech-video-box">
-<a href="https://www.youtube.com/watch?v=9No-FiEInLA" class="single-popup-wrap video-link">
-<div class="video-content">
-<div class="ht-popup-video video-button">
-<div class="video-mark">
-<div class="wave-pulse wave-pulse-1"></div>
-<div class="wave-pulse wave-pulse-2"></div>
-</div>
-<div class="video-button__two">
-<div class="video-play">
-<span class="video-play-icon"></span>
-</div>
-</div>
-</div>
-<div class="video-text">
-How we work
-</div>
-</div>
-</a>
-</div>
-</div>
+<h6 class="text-white">İşinizi Profesyonel Yazılım Çözümleriyle Dönüştürün </h6>
+<h1 class="font-weight--reguler text-white mb-30">Çanakkale, Türkiye'de lider web geliştirme şirketi <span class="text-line">Modern Teknolojilerle Oluşturulmuş Özel CRM, ERP, E-Ticaret ve Web Uygulamaları</span></h1>
 </div>
 </div>
 </div>
