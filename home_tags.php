83ac665174d30b906b430d68d0ec1976f045c0ed
hakan <hakan@hakan>
2026-02-22 18:51:02 +0300

    start 2026

--- home_tags.php
+++ home_tags.php
@@ -8,11 +8,12 @@
 <p>Hizmetlerimiz ve süreçlerimiz hakkında yaygın sorular</p>
 </div>
 
-<div class="tagcloud">
-<a href="javascript:void(0);" class="tag-cloud-link tag-link-4 tag-link-position-1" style="font-size: 22pt;" aria-label="business (7 items)">business</a>
-<a href="javascript:void(0);" class="tag-cloud-link tag-link-40 tag-link-position-2" style="font-size: 8pt;" aria-label="featured (1 item)">featured</a>
-<a href="javascript:void(0);" class="tag-cloud-link tag-link-2 tag-link-position-3" style="font-size: 22pt;" aria-label="IT Security (7 items)">IT Security</a>
-<a href="javascript:void(0);" class="tag-cloud-link tag-link-3 tag-link-position-4" style="font-size: 20.833333333333pt;" aria-label="IT services (6 items)">IT services</a></div>
+<a href="#" class="ht-btn ht-btn-md ht-btn--outline">Solid Style</a>
+<a href="#" class="ht-btn ht-btn-md ht-btn--outline">Solid Style</a>
+<a href="#" class="ht-btn ht-btn-md ht-btn--outline">Solid Style</a>
+<a href="#" class="ht-btn ht-btn-md ht-btn--outline">Solid Style</a>
+<a href="#" class="ht-btn ht-btn-md ht-btn--outline">Solid Style</a>
+<a href="#" class="ht-btn ht-btn-md ht-btn--outline">Solid Style</a>
 
 </div>
 
