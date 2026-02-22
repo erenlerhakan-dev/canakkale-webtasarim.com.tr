848c0475c52f6fcadd7640b3aeb2c0aa09b52cdd
hakan <hakan@hakan>
2026-02-22 18:52:32 +0300

    start 2026

--- home_tags.php
+++ home_tags.php
@@ -4,8 +4,8 @@
 <div class="col-lg-8 align-self-center">
 
 <div class="section-elements-title mb-30 text-center">
-<h4>Sık Sorulan Sorular</h4>
-<p>Hizmetlerimiz ve süreçlerimiz hakkında yaygın sorular</p>
+<h4>Kullandığımız Teknolojiler</h4>
+<p>Son teknoloji çözümler için modern teknoloji altyapıları</p>
 </div>
 
 <a href="#" class="ht-btn ht-btn-md ht-btn--outline">HTML5 & CSS3</a>
